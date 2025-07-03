5af046c62c2ca96525cfb3aacdf927add0f2c71c
Ubaid Ahmad <ahmadubaidedu@gmail.com>
2025-07-03 15:44:54 +0500

    Add files via upload

---
+++ day 2 fundamentals part 1/assignment 1/script.js
@@ -0,0 +1,8 @@
+alert('go to console for result!');
+let country='Pakistan';
+let population=247000000;
+let continent='Asia';
+
+console.log('country is:',country);
+console.log('population is:',population);
+console.log('Continent is:',continent);
---
+++ day 2 fundamentals part 1/assignment 3/script.js
@@ -0,0 +1,10 @@
+alert('go to console for result!');
+const country='Pakistan';
+let population=247000000;
+const continent='Asia';
+let language;
+
+language='pashto';
+// trying to change the value of constant "continent"
+continent='europe';
+
