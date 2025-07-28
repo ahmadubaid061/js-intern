746560b42354b488efd29124f27d10cf3a68f24e
Ubaid Ahmad <ahmadubaidedu@gmail.com>
2025-07-28 11:02:18 +0500

    updating array distructing with swaping values and rest perimeter.

--- day 12.1 destructuring arrays & objects and spread operator/array distructuring/script.js
+++ day 12.1 destructuring arrays & objects and spread operator/array distructuring/script.js
@@ -1,15 +1,29 @@
 "use strict";
 
 const arr = [1, 2, 3, [4, 5]];
-// distructuring
+/////////////////////////////////////////////// distructuring
 const [a, b, c, d] = arr;
 console.log(a, b, c, d);
 // outpput        1 2 3 [ 4, 5 ]
+/////////////////////////////////////////////////nested arrays
 const [e, f, g, [h, i]] = arr;
 console.log(e, f, g, h, i);
 //output           1 2 3 4 5
 
-// function returning more than one values
+///////////////////////////////////////////////...rest perimeter
+let aRR = [1, 2, 3, 4, 5];
+const [first, ...rest] = aRR;
+console.log("first is: ", first);
+console.log("rest is:  ", rest); // returns rest of the array values other than first
+
+///////////////////////////////////////////////////swaping values
+let q = 5;
+let p = 3;
+[q, p] = [p, q];
+console.log("q now is: ", q);
+console.log("p now is: ", p);
+
+////////////////////////////// function returning more than one values
 const array = [
   ["GLi", "XLi", "BMW", "ferrari"],
   ["salad", "Biryani", "noodles", "chicken"],
