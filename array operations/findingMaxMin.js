945646d02936f0bf6097b860d85425615a0d379b
Ubaid Ahmad <ahmadubaidedu@gmail.com>
2025-08-13 00:40:20 +0500

    added funciton findMaximumUsingReduce to script file

--- array operations/findingMaxMin.js
+++ array operations/findingMaxMin.js
@@ -9,6 +9,15 @@ function findMax(...rest) {
   }
   return max;
 }
+///////////////////////////////////------------------------finding maximum Value using reduced----------------------------
+
+const max = array.reduce((acc, curr) => {
+  if (acc > curr) {
+    return acc;
+  } else {
+    return curr;
+  }
+});
 
 ///////////////////////////////finding Minimum value///////////////////////
 function findMin(...rest) {
@@ -23,6 +32,9 @@ function findMin(...rest) {
 
 const numbers = [2, 4, 7, 1, 5, 9, 2, 4, 6, 4, 2];
 let maximum = findMax(...numbers);
+let maximumUsingReduce=max(numbers);
 let minimum = findMin(...numbers);
 console.log("Maximum number in the array is: ", maximum);
+console.log('Maximum number using reduce function is: ',maximumUsingReduce);
 console.log("Minimum number in the array is: ", minimum);
+
