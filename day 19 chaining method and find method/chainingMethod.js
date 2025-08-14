3e06bea5e6b68c36518b112edeec7d1dda9ac921
Ubaid Ahmad <ahmadubaidedu@gmail.com>
2025-08-14 10:46:11 +0500

    Update chainingMethod.js

--- day 19 chaining method and find method/chainingMethod.js
+++ day 19 chaining method and find method/chainingMethod.js
@@ -5,9 +5,10 @@ const calTotalDepositsUSD = function (array) {
   return array
     .filter((value) => value > 0) //to check if deposit
     .map((value) => value * 1.1) //to convert euro to USD
-    .reduce((acc, curr) => acc + curr); //to add all the movements
+    .reduce((acc, curr) => acc + curr,0); //to add all the movements
 };
 
 
 const movements=[230,140,-324,-894,8632,723,-98732,673,-793,792,729,872,-231];
 console.log('the total deposits in USD are: ',calTotalDepositsUSD(movements));
+
