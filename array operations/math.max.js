bb6ee2d3ca9ba54100bce629724f407c2380226f
Ubaid Ahmad <ahmadubaidedu@gmail.com>
2025-08-18 13:34:52 +0500

    Update math.max.js

--- array operations/math.max.js
+++ array operations/math.max.js
@@ -2,4 +2,6 @@
 
 const array = [2, 67, 34, 98, 24, 56, 13, 45, 58];
 
-console.log(Math.max(...array));
+console.log(Math.max(...array));  //returns 98
+console.log(Math.min(...array));  //returns 2
+
