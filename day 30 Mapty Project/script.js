f3affd6624d6061076e2ba535266eb3ad50f971c
Ubaid Ahmad <ahmadubaidedu@gmail.com>
2025-09-12 20:11:55 +0500

    Update script.js  Displayed a Map Using Leaflet Library

--- day 30 Mapty Project/script.js
+++ day 30 Mapty Project/script.js
@@ -30,11 +30,30 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       //now creating URL from these coords
-      const url = `https://www.google.com/maps?q=${latitude},${longitude}`; //this gives us the current location url
-      console.log(url);
+      //const url = `https://www.google.com/maps?q=${latitude},${longitude}`; //this gives us the current location url
+      //now to display the map based on these coordinates we need leaflet library --------copying code from there website
+      //changing var to const
+      //the L.map(here the string should be the id name of the div containing map)
+      //const map = L.map("map").setView([51.505, -0.09], 13);
+      //inside setview the [51.505, -0.09] are latitude and longitue so by modifying that
+      //the last value set to 13 is the zoom level of the map
+      const coordinates = [latitude, longitude];
+      const map = L.map("map").setView(coordinates, 13);
+      //-------------------------------the map layout design can be changed by L.tileLayer below,,,
+      //L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" ---just another layout
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+      //replacing [51.5, -0.09] with coordinates array for placing marker on the current location
+      L.marker(coordinates)
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
     },
     function () {
       alert('we couldn"t get your current location');
     }
   );
 }
+
