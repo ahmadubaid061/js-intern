9d87141bac0d4b65fa7ae1d52cf3cc3d117bf4bd
Ubaid Ahmad <ahmadubaidedu@gmail.com>
2025-09-16 15:54:34 +0500

    Update script.js rewrote entire code inside a class for clarity

--- day 30 Mapty Project/script.js
+++ day 30 Mapty Project/script.js
@@ -20,90 +20,187 @@ const input_distance = document.querySelector(".form__input--distance");
 const input_duration = document.querySelector(".form__input--duration");
 const input_cadence = document.querySelector(".form__input--cadence");
 const input_elevation = document.querySelector(".form__input--elevation");
-let map,mapEvent;
-//--------------------------------------------------------------------------------------------function for clearing and closing form when needed---------------------------
-const clearForm=function() {
-  input_cadence.value = "";
-  input_distance.value = "";
-  input_duration.value = "";
-  input_elevation.value = "";
-  input_type.value = "running";
-  form.classList.add("hidden");
-}
-
-//-----------------------------------------------------------------------------------------finding current position coordinates using geoLocation API------------------------
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const coords = position.coords; // this gives us the coordinates of th current position
-      //const latitude = position.coords.latitude; //this gives us the latitude
-      //i ma using destructuring
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      //now creating URL from these coords
-      const url = `https://www.google.com/maps?q=${latitude},${longitude}`; //this gives us the current location url
-      //--------------------------------------------------------------------------------------------------using Leaflet library to display map based on current position-----
-      //now to display the map based on these coordinates we need leaflet library --------copying code from there website
-      //changing var to const
-      //the L.map(here the string should be the id name of the div containing map)
-      //const map = L.map("map").setView([51.505, -0.09], 13);
-      //inside setview the [51.505, -0.09] are latitude and longitue so by modifying that
-      //the last value set to 13 is the zoom level of the map
-      const coordinates = [latitude, longitude];
-      map = L.map("map").setView(coordinates, 13);
-      //------------------------------------------------------------------------------------------------------the map layout design can be changed by L.tileLayer below,,,
-      //L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" ---just another layout
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-      //replacing [51.5, -0.09] with coordinates array for placing marker on the current location
-      // L.marker(coordinates) //takes coordinates array [latitude,longitude]
-      //   .addTo(map)
-      //   .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-      //   .openPopup();
-
-      //-------------------------------------------------------------------------------------------------------------------- diplaying a form on clicking the map------------
-      map.on("click", function (Event) {
-        mapEvent=Event;   //this is needed in form submition function
-         //-------------------------------------------------------------------------------------------------------------------now displaying form
-        form.classList.remove("hidden");
-        input_distance.focus();//curser is focused on distance field
-      });
-    },
-    function () {
-      alert('we couldn"t get your current location');
-    }
-  );
-}
-
-////-------------------------------------------------------------------------------------------------------------- diplaying marker on form submission----------------------
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  //the Event(when we click on map ) has a prototype peoperty "latlng" which contaains latitude and longitude
-  const { lat, lng } = mapEvent.latlng; //gives us latitude and longitude by destructuring
-  L.marker([lat, lng]) //it takes coordinates in the form of an array
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: true,
-        closeOnClick: false,
-        className: "running-popup",
-        // openOnHover: true,
-      })
-    )
-    .setPopupContent("workout")
-    .openPopup(); //calls the popup to deisplay a message
+// let map,mapEvent;
+//---------------------------------------------------------------------------------------creating a class App for handling all the combined events for cycling and running
+class App {
   
-  clearForm();
-});
-//-----------------------------------------------------------------------------------changing input type changes input fields(elevation <->cadence)----------------------
-input_type.addEventListener("change", function () {
-  input_elevation.closest(".form__row").classList.toggle("form__row--hidden");
-  input_cadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition(); //called immediatly after the document loads
+    input_type.addEventListener("change", this._toggleElevation);
+    form.addEventListener("submit", this._newWorkout.bind(this));
+  }
+  //----------------------------------------------------------------------------------------------------------------class method-1 for get current position
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('we couldn"t get your current location');
+        }
+      );
+    }
+  }
+  //----------------------------------------------------------------------------------------------------------------toggle method-2 for elevatio vs cadence
+  _toggleElevation() {
+    input_elevation.closest(".form__row").classList.toggle("form__row--hidden");
+    input_cadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+ //----------------------------------------------------------------------------------------------------------------new workout method-3 is added on submitting form
+  _newWorkout(e) {
+    e.preventDefault();
+
+    const { lat, lng } = this.#mapEvent.latlng; //prototype property of the mapEvent
+    // L.marker(array of coordinates)
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: true,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("workout")
+      .openPopup(); //calls the popup to deisplay a message
+
+    this._clearForm();
+  }
+  //--------------------------------------------------------------------------------------------------------method-4 creating and desplaying map using leaflet library
+  _loadMap(position) {
+    const coords = position.coords;
+
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    //const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+    const coordinates = [latitude, longitude];
+    //--------------------------------------------constructing map
+    // this.#map = L.map("map").setView([latitude,longitude], 13);
+    this.#map = L.map("map").setView(coordinates, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // diplaying a marker on click
+    this.#map.on("click", this._showform.bind(this)); 
+    //the event listener sets the this keyword to map and the bind is used to reset it to the App clas
+  }
+//------------------------------------------------------------------------------------------------------------------------method-5 show form on clicking on map anywhere
+  _showform(e) {
+    this.#mapEvent = e;
+
+    form.classList.remove("hidden");
+    input_distance.focus(); //curser is focused on distance field
+  }
+  //----------------------------------------------------------------------------------------------------------------------method-6 to clear form when it is submitted
+  _clearForm() {
+    input_cadence.value = "";
+    input_distance.value = "";
+    input_duration.value = "";
+    input_elevation.value = "";
+    input_type.value = "running";
+    form.classList.add("hidden");
+  }
+}
+//-----------------------------------------------------------------------creating a real object from the App class
+const app = new App();
+//here is no need to call the methods manually as they are already called inside constructor inside class App
+
+//=====//===============================================the starter  code without using classes
+// // // // // // // // // // // // // // // // // // // // //  // // // // // // // // // // // // // // // /
+
+// //--------------------------------------------------------------------------------------------function for clearing and closing form when needed---------------------------
+// const clearForm=function() {
+//   input_cadence.value = "";
+//   input_distance.value = "";
+//   input_duration.value = "";
+//   input_elevation.value = "";
+//   input_type.value = "running";
+//   form.classList.add("hidden");
+// }
+
+// //-----------------------------------------------------------------------------------------finding current position coordinates using geoLocation API------------------------
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//       const coords = position.coords; // this gives us the coordinates of th current position
+//       //const latitude = position.coords.latitude; //this gives us the latitude
+//       //i ma using destructuring
+//       const { latitude } = position.coords;
+//       const { longitude } = position.coords;
+//       //now creating URL from these coords
+//       const url = `https://www.google.com/maps?q=${latitude},${longitude}`; //this gives us the current location url
+//       //--------------------------------------------------------------------------------------------------using Leaflet library to display map based on current position-----
+//       //now to display the map based on these coordinates we need leaflet library --------copying code from there website
+//       //changing var to const
+//       //the L.map(here the string should be the id name of the div containing map)
+//       //const map = L.map("map").setView([51.505, -0.09], 13);
+//       //inside setview the [51.505, -0.09] are latitude and longitue so by modifying that
+//       //the last value set to 13 is the zoom level of the map
+//       const coordinates = [latitude, longitude];
+//       map = L.map("map").setView(coordinates, 13);
+//       //------------------------------------------------------------------------------------------------------the map layout design can be changed by L.tileLayer below,,,
+//       //L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" ---just another layout
+//       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//         attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//       }).addTo(map);
+//       //replacing [51.5, -0.09] with coordinates array for placing marker on the current location
+//       // L.marker(coordinates) //takes coordinates array [latitude,longitude]
+//       //   .addTo(map)
+//       //   .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+//       //   .openPopup();
+
+//       //-------------------------------------------------------------------------------------------------------------------- diplaying a form on clicking the map------------
+//       map.on("click", function (Event) {
+//         mapEvent=Event;   //this is needed in form submition function
+//          //-------------------------------------------------------------------------------------------------------------------now displaying form
+//         form.classList.remove("hidden");
+//         input_distance.focus();//curser is focused on distance field
+//       });
+//     },
+//     function () {
+//       alert('we couldn"t get your current location');
+//     }
+//   );
+// }
+
+// ////-------------------------------------------------------------------------------------------------------------- diplaying marker on form submission----------------------
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   //the Event(when we click on map ) has a prototype peoperty "latlng" which contaains latitude and longitude
+//   const { lat, lng } = mapEvent.latlng; //gives us latitude and longitude by destructuring
+//   L.marker([lat, lng]) //it takes coordinates in the form of an array
+//     .addTo(map)
+//     .bindPopup(
+//       L.popup({
+//         maxWidth: 250,
+//         minWidth: 100,
+//         autoClose: true,
+//         closeOnClick: false,
+//         className: "running-popup",
+//         // openOnHover: true,
+//       })
+//     )
+//     .setPopupContent("workout")
+//     .openPopup(); //calls the popup to deisplay a message
+  
+//   clearForm();
+// });
+// //-----------------------------------------------------------------------------------changing input type changes input fields(elevation <->cadence)----------------------
+// input_type.addEventListener("change", function () {
+//   input_elevation.closest(".form__row").classList.toggle("form__row--hidden");
+//   input_cadence.closest(".form__row").classList.toggle("form__row--hidden");
+// });
+
 
 
 
